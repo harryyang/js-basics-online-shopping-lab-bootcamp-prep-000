930727224116c746026ca9c8ec1f939ff9f40153
Harold Yang <harold.h.yang@gmail.com>
2017-09-22 17:51:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100);
+  cart.push ({[item]: price})
+  console.log (`${item} has been added to your cart.`)
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+    return
+  }
+  var statement = 'In your cart, you have '
+  for (var i = 0; i < cart.length; i++) {
+    var item = Object.keys(cart[i]);
+      statement += item + " at $" + cart[i][item] + ", "
+  }
+  statement = statement.slice(0,-2)
+  statement += '.'
+  console.log(statement);
+  return
 }
 
 function total() {
-  // write your code here
+  let t = 0
+  for (var i = 0; i < cart.length; i++) {
+    for (var item in cart[i]) {
+      t += cart[i][item]
+    }
+  }
+  return t
 }
 
 function removeFromCart(item) {
